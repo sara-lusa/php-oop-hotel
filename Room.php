74b70b50cfbfbc9333a88966b41dd2798d76962f
Sara <saracorsob@gmail.com>
2020-08-29 13:27:01 +0200

    Scrittura e stampa istanze classe Room

--- Room.php
+++ Room.php
@@ -1,25 +1,24 @@
 <?php
-class Room {
-  private $room_id;
-  private $room_number;
-  private $floor;
-  private $beds;
+class Room{
+  public $room_id;
+  public $room_number;
+  public $floor;
+  public $beds;
 
   public function __construct($_room_id, $_room_number, $_floor, $_beds) {
     $this->room_id = $_room_id;
     $this->room_number = $_room_number;
     $this->floor = $_floor;
     $this->beds = $_beds;
-  };
+  }
 
-  public getRoomData() {
+  public function getRoomData() {
     return [
-      'id' = $this->room_id,
-      'number' = $this->room_number,
-      'floor' = $this->floor,
-      'beds' = $this->beds,
-    ]
-  };
+      'id' => $this->room_id,
+      'number' => $this->room_number,
+      'floor' => $this->floor,
+      'beds' => $this->beds,
+    ];
+  }
 }
-
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,35 @@
 <?php
-// require_once(__DIR__ . '/Room.php');
+require_once(__DIR__ . '/Room.php');
 
-echo 'Ciao';
+$stanza_uno = new Room(1, 100, 1, 2);
+$array_data = $stanza_uno->getRoomData();
+?>
+<h3>Rooms</h3>
+<ul>
+  <li><h3>Room n. <?php echo $array_data['number']; ?></h3></li>
+  <li>Id: <?php echo $array_data['id']; ?></li>
+  <li>Floor: <?php echo $array_data['floor']; ?></li>
+  <li>Beds: <?php echo $array_data['beds']; ?></li>
+<?php
+
+$stanza_due = new Room(2, 101, 1, 3);
+$array_data = $stanza_due->getRoomData();
+?>
+<li><h3>Room n. <?php echo $array_data['number']; ?></h3></li>
+<li>Id: <?php echo $array_data['id']; ?></li>
+<li>Floor: <?php echo $array_data['floor']; ?></li>
+<li>Beds: <?php echo $array_data['beds']; ?></li>
+<?php
+
+$stanza_tre = new Room(3, 102, 1, 3);
+$array_data = $stanza_tre->getRoomData();
+?>
+<li><h3>Room n. <?php echo $array_data['number']; ?></h3></li>
+<li>Id: <?php echo $array_data['id']; ?></li>
+<li>Floor: <?php echo $array_data['floor']; ?></li>
+<li>Beds: <?php echo $array_data['beds']; ?></li>
+</ul>
+<?php
 
 
 ?>
